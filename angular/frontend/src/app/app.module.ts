62f0c29c81411672e3d5e4c5fa0166ccdf6482af
TelmiAdame <telmiarquit@hotmail.com>
2021-09-27 11:37:55 -0300

    creation of the component-update of the product and its route through the edit icon

--- angular/frontend/src/app/app.module.ts
+++ angular/frontend/src/app/app.module.ts
@@ -31,7 +31,8 @@ import { MatSortModule } from '@angular/material/sort';
 
 
 import localePt from '@angular/common/locales/pt'
-import { registerLocaleData } from '@angular/common'
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
 
 registerLocaleData(localePt);
 
@@ -45,6 +46,7 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
